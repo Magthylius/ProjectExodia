504e52416e0052ded9c9c2ee7952ea1d3478ba5e
Magthylius <56510076+Magthylius@users.noreply.github.com>
2023-07-29 00:36:19 +0800

    refactor: convert to enemy entity

--- ProjectExodia/Assets/_Project/Scripts/SpawnManager/EnemyEntity.cs
+++ ProjectExodia/Assets/_Project/Scripts/SpawnManager/EnemyEntity.cs
@@ -6,7 +6,23 @@ namespace ProjectExodia
     public class EnemyEntity : MonoBehaviour, ISlappableEntity
     {
         [SerializeField] private MMF_Player effectsPlayer;
+        [SerializeField] private float despawnOffset = 10.0f;
+
+        private Transform _playerTransform;
         
+        private void Update()
+        {
+            if (_playerTransform && _playerTransform.position.z - despawnOffset > transform.position.z)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void Initialize(Transform playerTransform)
+        {
+            _playerTransform = playerTransform;
+        }
+
         public void PerformSlap()
         {
             effectsPlayer.PlayFeedbacks();
--- ProjectExodia/Assets/_Project/Scripts/SpawnManager/SpawnManager.cs
+++ ProjectExodia/Assets/_Project/Scripts/SpawnManager/SpawnManager.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,15 +6,17 @@ namespace ProjectExodia
 {
     public class SpawnManager : ManagerBase
     {
-        [SerializeField] private GameObject[] enemyPrefabs;
+        [SerializeField] private EnemyEntity[] enemyEntityPrefabs;
         [SerializeField] private float spawnDistance = 70;
         [SerializeField] private float spawnGap = 4;
         [SerializeField] private float spawnTime;
         [SerializeField] private float yPositionOffset;
 
-        private readonly List<GameObject> _activeEnemies = new();
+        private readonly List<EnemyEntity> _activeEnemies = new();
         private TileManager _tileManager;
-        
+        private PlayerManager _playerManager;
+
+        private float _ySpawnPosition;
         private float _timerElapsed;
         private int _lastPrefabIndex = 0;
 
@@ -31,6 +30,13 @@ namespace ProjectExodia
                 gameObject.SetActive(false);
                 return;
             }
+            
+            if (!gameContext.TryGetManager(out _playerManager))
+            {
+                Debug.LogError($"{this}: Failed to get player manager. Disabling {this}'s game object.");
+                gameObject.SetActive(false);
+                return;
+            }
 
             if (!GameContext.TryGetManager<TimerManager>(out var timerManager))
             {
@@ -40,34 +46,37 @@ namespace ProjectExodia
             }
             
             timerManager.CreateTimer(SpawnEnemy, spawnTime, true);
+            _ySpawnPosition =_tileManager.transform.position.y + yPositionOffset;
         }
 
         private void SpawnEnemy()
         {
-            var goEnemy = Instantiate(enemyPrefabs[RandomPrefabIndex()], transform, true);
+            if (enemyEntityPrefabs.Length <= 0) return;
+            
+            var enemy = Instantiate(enemyEntityPrefabs[RandomPrefabIndex()], transform, true);
+            enemy.Initialize(_playerManager.Controller.PlayerTransform);
             var randomGap = Random.Range(-spawnGap, spawnGap);
-
-            goEnemy.transform.position = new Vector3(randomGap, (_tileManager.transform.position.y + yPositionOffset),
-                _tileManager.GetLastSpawnLocation().z);
-            _activeEnemies.Add(goEnemy);
+            
+            enemy.transform.position = new Vector3(randomGap, _ySpawnPosition, _tileManager.GetLastSpawnLocation().z);
+            _activeEnemies.Add(enemy);
         }
 
-        private void DeleteEnemy(GameObject pEnemyToRemove)
+        private void DeleteEnemy(EnemyEntity enemy)
         {
-            var enemyToRemove = _activeEnemies.Find(obj => obj == pEnemyToRemove);
-            if (enemyToRemove == null) return;
-            Destroy(enemyToRemove, 5.0f);
-            _activeEnemies.Remove(pEnemyToRemove);
+            if (!_activeEnemies.Contains(enemy)) return;
+            Destroy(enemy.gameObject, 5.0f);
+            
+            _activeEnemies.Remove(enemy);
         }
         
         private int RandomPrefabIndex()
         {
-            if (enemyPrefabs.Length <= 1) return 0;
+            if (enemyEntityPrefabs.Length <= 1) return 0;
 
             var randomIndex = _lastPrefabIndex;
             
             while (randomIndex == _lastPrefabIndex)
-                randomIndex = Random.Range(0, enemyPrefabs.Length);
+                randomIndex = Random.Range(0, enemyEntityPrefabs.Length);
 
             _lastPrefabIndex = randomIndex;
             return randomIndex;
