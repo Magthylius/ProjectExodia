39c449345f771615bbc48ba66a5f4dc2aa98131c
Magthylius <56510076+Magthylius@users.noreply.github.com>
2023-07-30 10:57:55 +0800

    feat: passport stamping

--- ProjectExodia/Assets/_Project/Scripts/UI/EffectsPanel.cs
+++ ProjectExodia/Assets/_Project/Scripts/UI/EffectsPanel.cs
@@ -2,12 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ProjectExodia
 {
     public class EffectsPanel : MenuPanel
     {
         [SerializeField] private GameObject goBananaFireObject;
+        [SerializeField] private GameObject passportStampingObject;
+        
+        [SerializeField] private Image passportStampImage;
+        [SerializeField] private Sprite alaskaStampSprite;
+        [SerializeField] private Sprite antarcticaStampSprite;
+        [SerializeField] private Sprite indiaStampSprite;
+        [SerializeField] private Sprite japanStampSprite;
+        [SerializeField] private Sprite malaysiaStampSprite;
+        
 
         private void Awake()
         {
@@ -15,5 +25,21 @@ namespace ProjectExodia
         }
 
         public void SetGoBananaFire(bool active) => goBananaFireObject.SetActive(active);
+
+        public void SetPassportStamping(bool active, int regionType = -1)
+        {
+            passportStampingObject.SetActive(active);
+            if (active)
+            {
+                switch (regionType)
+                {
+                    case 0: passportStampImage.sprite = alaskaStampSprite; break;
+                    case 1: passportStampImage.sprite = antarcticaStampSprite; break;
+                    case 2: passportStampImage.sprite = indiaStampSprite; break;
+                    case 3: passportStampImage.sprite = japanStampSprite; break;
+                    case 4: passportStampImage.sprite = malaysiaStampSprite; break;
+                }
+            }
+        }
     }
 }
